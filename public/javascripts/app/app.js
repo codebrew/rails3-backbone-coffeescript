cd49a9244564c0b9f36ba733628c517758cb1a2c
Ryan Fitzgerald <ryan@gymlogr.com>
2011-03-18 13:57:41 -0400

    breaking files out

---
+++ public/javascripts/app/app.js
@@ -0,0 +1,18 @@
+/* DO NOT MODIFY. This file was compiled Fri, 18 Mar 2011 17:44:55 GMT from
+ * /Users/fitz/Projects/rails3-backbone-coffeescript/app/coffeescripts/app.coffee
+ */
+
+(function() {
+  window.App = {
+    Models: {},
+    Controllers: {},
+    Views: {},
+    currentUser: function(user) {
+      if (user) {
+        return this.user = new App.Models.User(user);
+      } else {
+        return this.user;
+      }
+    }
+  };
+}).call(this);
