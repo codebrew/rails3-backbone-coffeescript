a64bc5b690c89d0492c99060c16d034d0256647d
Ryan Fitzgerald <ryan@gymlogr.com>
2011-03-18 14:00:05 -0400

    making sass never compile while in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Rails3BackboneCoffeescript::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # dont compile sass because heroku is read only
+  Sass::Plugin.options[:never_update] = true
 end
